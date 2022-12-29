35c69a1bd63a183077ef22b3d79074e7f159dcfe
Shodhan <207r1a6660@cmrtc.ac.in>
2022-12-29 21:11:13 +0530

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,41 @@ import Axios from "axios";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Piechart from "./pages/piechart";
+import Barchart from "./pages/barcharts";
+import { ThemeProvider } from "react-bootstrap";
 
 function App() {
   const [programmingLanguagesKnown, setprogrammingLanguagesKnown] = useState<any>([]);
   const [handle,setHandle]=useState("");
   const [verdicts,setVerdicts]=useState([]);
   const[tags,setTags]=useState([]);
+  const[levels,setLevels]=useState([]);
+  const[rating,setRating]=useState([]);
 
   //Fetching the data from API
   const fetchApi = () => {
     Axios.get("https://codeforces.com/api/user.status?handle="+handle).then((res) => {
 
       let resultLength=res.data.result.length;
-      
       let programmingLanguages=[""];
       let verdictArray=[""];
       let tagsArray=[""];
+      let levelsArray=[""];
+      let ratingArray=[""];
 
       for (let i = 0; i < resultLength; i++){
         programmingLanguages=[...programmingLanguages,res.data.result[i].programmingLanguage];
         verdictArray=[...verdictArray,res.data.result[i].verdict];
         tagsArray=[...tagsArray,...res.data.result[i].problem.tags];
+        levelsArray=[...levelsArray,res.data.result[i].problem.index];
+        ratingArray=[...ratingArray,res.data.result[i].problem.rating];
+
       }
       function fetchData(data:any):any{
-      const dataMap = data.reduce((acc:any, e:any) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+      const dataMap = data.reduce((accumualtor:any, entry:any) => accumualtor.set(entry, (accumualtor.get(entry) || 0) + 1), new Map());
       let tmp=[{}];
       tmp.shift();
-      console.log(dataMap);
+      // console.log(dataMap);
       dataMap.forEach(function(value:any, key:any) {
 
         if(key!==""){
@@ -45,6 +53,17 @@ function App() {
           tmp=[p];
         }
       })
+      function compare(a:any, b:any) {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      }
+      
+      tmp.sort(compare);
       return tmp;
       }
 
@@ -57,6 +76,11 @@ function App() {
       //Tags
       setTags(fetchData(tagsArray));
 
+      //Levels
+      setLevels(fetchData(levelsArray));
+
+      //Rating
+      setRating(fetchData(ratingArray));
 
     })
     .catch(function (error) {
@@ -66,8 +90,17 @@ function App() {
       {}
     });
   };
-
-
+  if(!handle){
+    return <>
+    <NavBar/>
+    <div className="form1" >
+    
+    <Form>
+      <Form.Control type="text" onChange={(e) => setHandle(e.target.value)} placeholder="Enter Codeforces Handle" />
+      <Button variant="primary" onClick={fetchApi} className="submit">Submit</Button>
+    </Form>
+    </div></>;
+  }
 
 
   return (
@@ -80,6 +113,7 @@ function App() {
       <Button variant="primary" onClick={fetchApi} className="submit">Submit</Button>
     </Form>
     </div>
+    {/* {DisPlaying The PieCharts} */}
       <div>
       <div className="d-flex justify-content-around">
             <div className="shadow-lg p-3 mb-5 bg-white rounded">
@@ -91,12 +125,23 @@ function App() {
       </div>
       <div className="d-flex justify-content-around">
       <div className="shadow-lg p-3 mb-5 bg-white rounded">
-          <Piechart data={tags} cx={300} cy={300} width={800} height={600} outerRadius={250} innerRadius={100} handle={handle} label="Tags of "/>
+          <Piechart data={tags} cx={300} cy={300} width={800} height={600} outerRadius={280} innerRadius={150} handle={handle} label="Tags of "/>
       </div>
       </div>
-      <p>{handle}</p>
+      
+      
+      
     </div>
-    
+    <div className="d-flex justify-content-around">
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
+        <Barchart label={"Problem levels of"} handle={handle} data={levels}/>
+        </div>
+    </div>
+    <div className="d-flex justify-content-around">
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
+        <Barchart label={"Problem ratings of"} handle={handle} data={rating}/>
+        </div>
+    </div>
     </>
   );
 }
--- src/pages/piechart.tsx
+++ src/pages/piechart.tsx
@@ -6,27 +6,8 @@ const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042","#8FCF47","#b1faf2"];
 
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index
-}: any) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-  return (
-    
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"}dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-export default function Languages(props:any) {
+export default function Piechart(props:any) {
   if(!props.data){
     return <></>;
   }
